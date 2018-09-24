24d357361d06a6b90eb01937e796441248cce411
JoshCplusplus <joshcomes@gmail.com>
2018-09-24 10:33:17 -0700

    Finished one link with uploading to sql database

--- UCSDBS.py
+++ UCSDBS.py
@@ -5,12 +5,14 @@ from time import sleep
 import sqlite3
 import re
 
-conn = sqlite3.connect('test.db')
+#Create DataBase
+conn = sqlite3.connect('UCSDprices.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE if not exists bookstore
          (class text, book text, price real, condition text, retailer text)''')
 conn.commit()
 
+#Use Selenium and BeautifulSopu to scrape HTML
 browser = webdriver.Chrome(os.getcwd() + "/chromedriver.exe")
 url = "https://ucsd.verbacompare.com/comparison?id=68066"
 browser.get(url)
@@ -18,10 +20,12 @@ sleep(2)
 innerHTML = browser.execute_script("return document.body.innerHTML")
 page_soup = BeautifulSoup(innerHTML, "html.parser")
 
+#Create tuples and list to insert all data into DB
 sql_list = []
 sql_tuple = ()
 class_name = page_soup.find("div",{"class":"in_section"}).span.text.strip()
 
+#Pull the different books from the HTML and loop over them and their prices
 items = page_soup.findAll("div",{"class":"item_details"})
 for indivItem in items:
     name = indivItem.find("td",{"class":"title"}).text.strip()
@@ -29,6 +33,7 @@ for indivItem in items:
     condition = indivItem.findAll("div",{"class":"condition"})
     retailer = indivItem.findAll("div",{"class":"retailer"})
     for count, indivPrice in enumerate(price):
+        #Get just the float value of the price
         price_list = []
         for n in indivPrice.text.strip().replace("$",""):
             try:
@@ -37,7 +42,8 @@ for indivItem in items:
                 pass
         sql_tuple = (class_name, name, price_list[0], condition[count].text.strip(), retailer[count].text.strip())
         sql_list.append(sql_tuple)
-print(sql_list)
+#Uplaod everything to the database
 c.executemany('INSERT OR REPLACE INTO bookstore VALUES (?,?,?,?,?)', sql_list)
-c.close()
+conn.commit()
+conn.close()
 browser.close()
